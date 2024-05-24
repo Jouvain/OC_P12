b6dce307e93ce2e45b8234bf37a19ff438db2a5c
Cyril FERRER <cyril.ferrer31@gmail.com>
2024-05-24 17:21:29 +0200

    galerie fonctionnelle + pagination

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,11 +4,19 @@ import Button from "../Button/Button"
 import Navbar from "../Navbar/Navbar"
 import github from "../../assets/github.svg"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import "./Card.css"
 
 export default function Card({picture, name, linkGitHub, linkDemo}) {
     const [modal, setModal] = useState(false)
+    let demo;
+    let git;
 
+    if (linkDemo == "#") {demo= <Link to="/*" target="_blank"> <Button label="démo" /> </Link> }
+    else {demo = <a href={linkDemo} target="_blank"> <Button label="démo" /> </a> }
+    if (linkGitHub == "#") {git= <Link to="/*" target="_blank"> <img src={github} alt="github logo" className="card__icon"/> </Link>}
+    else {git= <a href={linkGitHub} target="_blank"> <img src={github} alt="github logo" className="card__icon"/> </a>   }
+    
     function opening() {
         setModal(true)
     }
@@ -23,8 +31,8 @@ export default function Card({picture, name, linkGitHub, linkDemo}) {
             <h3 className="card__title"> {name} </h3>
             <div className="card__links">
                 <button className="card__button" onClick={opening}> ? </button>
-                <a href={linkDemo}> <Button label="démo" /> </a>
-                <a href={linkGitHub}> <img src={github} alt="github logo" className="card__icon"/> </a>      
+                {demo}
+                {git}   
             </div>
 
         </article>
--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,18 +1,42 @@
 /* imports */
+import { useState } from "react"
 import Card from "../Card/Card"
 import projects from "../../utils/projects.json"
 import "./Gallery.css"
 
 export default function Gallery() {
+    const [currentPage, setCurrentPage] = useState(1)
+    const PER_PAGE = 4
+    const filteredProjects = (
+        projects.filter((project, index) => {
+            if( ((currentPage - 1)*PER_PAGE <= index && PER_PAGE*currentPage > index) ) {return true}
+            else {return false}
+        })
+    )
+    let pageNumber = Math.floor((filteredProjects.length || 0) / PER_PAGE) + 1;
+
+    function newPage(n) {
+        setCurrentPage(n+1)
+    }
+
     return(
+        <>
         <div className="gallery">
             {
-                projects.map((project)=> {
+                filteredProjects.map((project)=> {
                     return(
                         <Card key={project.id} picture={project.picture} name={project.name} linkGitHub={project.linkGitHub} linkDemo={project.linkDemo} />
                     )
                 })
             }
         </div>
+        <div className="pagination">
+            {
+                [...Array(pageNumber || 0)].map((_,n) => (
+                    <button key={n} onClick={()=> newPage(n)} > {n+1} </button>
+                ))
+            }
+        </div>
+        </>
     )
 }
