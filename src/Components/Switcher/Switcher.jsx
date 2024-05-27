d9465b5a372cc6ab6c0a80bded8b7db3631f3917
Cyril FERRER <cyril.ferrer31@gmail.com>
2024-05-27 20:24:58 +0200

    add flags for switcher

--- src/Components/Switcher/Switcher.jsx
+++ src/Components/Switcher/Switcher.jsx
@@ -1,5 +1,6 @@
 /** imports **/
-
+import french from "../../assets/french_flag.png"
+import english from "../../assets/british_flag.png"
 import "./Switcher.css"
 
 export default function Switcher({label, handleClick}) {
@@ -10,6 +11,8 @@ export default function Switcher({label, handleClick}) {
             <div className="toggle-switch">
                 <input type="checkbox" className="checkbox" name={label} id={label} />
                 <label htmlFor={label} className="label" onClick={handleClick}>
+                    <img src={french} className="french" />
+                    <img src={english} className="english" />
                     <span className="inner"/>
                     <span className="switch"/>
                 </label>         
